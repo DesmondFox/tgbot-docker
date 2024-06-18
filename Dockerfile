da31578969c94c6fabcc1a476b250dbdaf8eeb61
Alex <survare@gmail.com>
2024-06-18 22:17:12 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.10.14-alpine3.20
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# RUN the command to start the app
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,23 @@
+import datetime
+import telebot
+import os
+
+bot =  telebot.TeleBot(os.environ["TOKEN"])
+
+print("Bot is running: ", bot.get_me())
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Hello! I am a bot")
+
+@bot.message_handler(commands=['time'])
+def time(message):
+    bot.reply_to(message, "Time is: " + str(datetime.datetime.now()))
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    print("On message: ", message.text)
+
+
+bot.infinity_polling()
